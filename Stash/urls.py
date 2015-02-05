8ac70a3697c435f59eac1d287327dcaf6c7d0ada
maxwell <mjm698@nyu.edu>
2015-02-05 18:58:40 -0500

    refactored

--- Stash/urls.py
+++ Stash/urls.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'polymer.views.home', name='home'),
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- polymer/views.py
+++ polymer/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, render_to_response, RequestContext, HttpResponse
+from datetime import datetime
+from polymer.models import *
 
-# Create your views here.
+def home(request):
+    return render_to_response('index.html', {}, RequestContext(request))
